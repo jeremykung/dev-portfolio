e756419d18655be92bc165cc1b6ef17a0f90bb9d
Jeremy Kung <jeremykung@gmail.com>
2023-10-23 00:47:31 +0300

    reset app components

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
 	<div>
-		<Contact />
-		<Pagination />
 		<UInput
 			icon="i-heroicons-magnifying-glass-20-solid"
 			color="white"
@@ -9,11 +7,6 @@
 			name="input"
 			placeholder="Search..."
 		/>
-		<UButton icon="i-basil-battry-half-solid" />
-		<UIcon name="i-heroicons-magnifying-glass" />
-		<UIcon name="i-heroicons-moon" />
-		<Modal />
-		<Slideover />
 		<!-- <NuxtWelcome /> -->
 	</div>
 </template>
--- components/Pagination.vue
+++
@@ -1,8 +0,0 @@
-<template>
-	<UPagination v-model="page" :page-count="5" :total="items.length" />
-</template>
-
-<script setup>
-	const page = ref(1)
-	const items = ref(Array(55))
-</script>
